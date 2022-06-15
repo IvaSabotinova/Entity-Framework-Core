5e840e2d46857fe2741366838d2ec482068152d6
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-06-15 23:34:32 +0300

    Add files via upload

---
+++ 01.ADO.NET/T02VillainNames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace T02VillainNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = "Server = .; Database = MinionsDB; Integrated security = true;";
+            using SqlConnection sqlConnection = new SqlConnection(connection);
+            {
+                sqlConnection.Open();
+
+                using SqlCommand sqlCmd = new SqlCommand(@"SELECT v.[Name], COUNT(mv.MinionId) AS[CountOfMinions] FROM MinionsVillains AS mv JOIN Villains AS v ON mv.VillainId = v.Id
+                GROUP BY v.[Name] HAVING COUNT(mv.MinionId) > 3 ORDER BY CountOfMinions DESC", sqlConnection);
+
+                using SqlDataReader reader = sqlCmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{(string)reader["Name"]} - {(int)reader["CountOfMinions"]}");
+                }
+            }
+
+        }
+    }
+}
---
+++ 01.ADO.NET/T03MinionNames/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace T03MinionNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int villainIdAsInput = int.Parse(Console.ReadLine());
+            string connection = "Server = .; Database = MinionsDB; Integrated security = true;";
+
+            using SqlConnection sqlConnection = new SqlConnection(connection);
+            {
+                sqlConnection.Open();
+
+                using SqlCommand sqlCommand1 = new SqlCommand(@"SELECT Name FROM Villains
+                WHERE Id = @VillainId", sqlConnection);
+                sqlCommand1.Parameters.AddWithValue("@VillainId", villainIdAsInput);
+                string villainResult1 =(string) sqlCommand1.ExecuteScalar();
+
+                if (string.IsNullOrEmpty(villainResult1))
+                {
+                    Console.WriteLine($"No villain with ID {villainIdAsInput} exists in the database."); return;
+                }
+                else
+                {
+                    Console.WriteLine($"Villain: {villainResult1}");
+                }
+
+                using SqlCommand sqlCommand2 = new SqlCommand(@"SELECT COUNT(m.Id) FROM MinionsVillains AS mv JOIN Minions AS m ON mv.MinionId = m.Id WHERE mv.VillainId = @VillainId", sqlConnection);
+                sqlCommand2.Parameters.AddWithValue("@VillainId", villainIdAsInput);
+                 int villainResult2 = (int)sqlCommand2.ExecuteScalar();
+                if (villainResult2 == 0)
+                {
+                    Console.WriteLine("(no minions)");
+                }
+                else
+                {
+                    using SqlCommand sqlCommand3 = new SqlCommand(@"SELECT m.[Name] AS [MinionsNames], m.Age as [MinionAge] FROM MinionsVillains AS mv JOIN Minions AS m ON mv.MinionId = m.Id JOIN Villains AS v ON mv.VillainId = v.Id WHERE v.Id = @VillainId
+                      ORDER BY m.[Name]", sqlConnection);
+                    sqlCommand3.Parameters.AddWithValue("@VillainId", villainIdAsInput);
+                    SqlDataReader reader = sqlCommand3.ExecuteReader();
+
+                    int count = 1;
+                    while (reader.Read())
+                    {
+                        string villainsMinion = (string)reader["MinionsNames"];
+                        int minionAge = (int)reader["MinionAge"];
+                        Console.WriteLine($"{count}. {villainsMinion} {minionAge}");
+                        count++;
+                    }
+
+                }
+            }
+
+        }
+    }
+}
