757c694f1318e608b32ed484bb25c71cba9de1a7
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-06-27 15:29:12 +0300

    Add files via upload

---
+++ 02.ORM-Fundamentals/MiniORM/ChangeTracker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace MiniORM
+{
+    internal class ChangeTracker<T> where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            added = new List<T>();
+            removed = new List<T>();
+
+            allEntities = CloneEntities(entities);
+        }
+        private List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            List<T> clonedEntities = new List<T>();
+            PropertyInfo[] propertiesToClone = typeof(T).GetProperties().Where(pi =>
+            DbContext.AllowedSqlTypes.Contains(pi.PropertyType)).ToArray();
+
+            foreach (T entity in entities)
+            {
+                T clonedEntity = Activator.CreateInstance<T>();
+                foreach (PropertyInfo property in propertiesToClone)
+                {
+                    Object value = property.GetValue(entity);
+                    property.SetValue(clonedEntity, value);
+                }
+                clonedEntities.Add(clonedEntity);
+            }
+            return clonedEntities;
+        }
+
+        public IReadOnlyCollection<T> AllEntities => allEntities.AsReadOnly();
+
+        public IReadOnlyCollection<T> Added => added.AsReadOnly();
+
+        public IReadOnlyCollection<T> Removed => removed.AsReadOnly();
+
+        public void Add(T item) => this.added.Add(item);
+        public void Remove(T item) => this.removed.Add(item);
+
+        public IEnumerable<T> GetModifiedEntities(DbSet<T> dbSet)
+        {
+            List<T> modifiedEntities = new List<T>();
+            PropertyInfo[] primaryKeys = typeof(T).GetProperties().Where(pi => pi.HasAttribute<KeyAttribute>()).ToArray();
+            foreach (T proxyEntity in allEntities)
+            {
+                object[] primaryKeyValues = GetPrimaryKeyValues(primaryKeys, proxyEntity).ToArray();
+                T entity = dbSet.Entities.Single(e => GetPrimaryKeyValues(primaryKeys, e).SequenceEqual(primaryKeyValues));
+                bool isModified = IsModified(proxyEntity, entity);
+                if (isModified)
+                {
+                    modifiedEntities.Add(entity);
+                }
+            }
+            return modifiedEntities;
+
+        }
+
+        private static bool IsModified(T entity, T proxyEntity)
+        {
+            IEnumerable<PropertyInfo> monitoredProperties = typeof(T).GetProperties().Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType));
+            PropertyInfo[] modifiedProperties = monitoredProperties.Where(pi => !Equals(pi.GetValue(entity), pi.GetValue(proxyEntity))).ToArray();
+            bool isModified = modifiedProperties.Any();
+            return isModified;
+        }
+        private static IEnumerable<object> GetPrimaryKeyValues(IEnumerable<PropertyInfo> primaryKeys, T entity)
+        {
+            return primaryKeys.Select(pk => pk.GetValue(entity));
+        }
+    }
+}
---
+++ 02.ORM-Fundamentals/MiniORM/DbSet.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
+{
+    public class DbSet<TEntity> : ICollection<TEntity>
+    where TEntity : class, new()
+    {
+        internal ChangeTracker<TEntity> ChangeTracker { get; set; }
+        internal IList<TEntity> Entities { get; set; }
+
+        public int Count => this.Entities.Count;
+
+        public bool IsReadOnly => Entities.IsReadOnly;
+
+        public DbSet(IEnumerable<TEntity> entities)
+        {
+            Entities = entities.ToList();
+            ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+        public void Add(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentException(nameof(item), "Item cannot be null!");
+            }
+            Entities.Add(item);
+            ChangeTracker.Add(item);
+        }
+
+        public void Clear()
+        {
+            while (Entities.Any())
+            {
+                TEntity entity = Entities.First();
+                this.Remove(entity);
+            }
+        }
+
+        public bool Contains(TEntity item) => Entities.Contains(item);
+
+
+        public void CopyTo(TEntity[] array, int arrayIndex) => Entities.CopyTo(array, arrayIndex);
+
+        public bool Remove(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentException(nameof(item), "Item cannot be null!");
+            }
+            bool removedSuccessFully = Entities.Remove(item);
+
+            if (removedSuccessFully)
+            {
+                ChangeTracker.Remove(item);
+            }
+            return removedSuccessFully;
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return Entities.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities.ToArray())
+            {
+                this.Remove(entity);
+            }
+
+        }
+    }
+}
+
