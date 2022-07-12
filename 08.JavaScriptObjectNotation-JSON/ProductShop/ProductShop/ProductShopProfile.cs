4ecfd0f3f60f1ae0dbf638be58eb4c003ea6483b
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-07-12 14:42:47 +0300

    Add files via upload

---
+++ 08.JavaScriptObjectNotation-JSON/ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ProductShop.DataTransferObjects_DTOs_;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<UserInputModel, User>();
+            CreateMap<ProductInputModel, Product>();
+            CreateMap<CategoryInputModel, Category>();
+            CreateMap<CategoryProductInputModel, CategoryProduct>();    
+                
+        }
+    }
+}
