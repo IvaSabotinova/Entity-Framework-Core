f1d002cb153c8be08a047fc0df46d87ce1276bf7
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-07-24 15:50:15 +0300

    Delete 08.JavaScriptObjectNotation-JSON/ProductShop directory

--- 08.JavaScriptObjectNotation-JSON/ProductShop/ProductShop/DataTransferObjects(DTOs)/CategoryProductInputModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductShop.DataTransferObjects_DTOs_
-{
-    public class CategoryProductInputModel
-    {
-        public int CategoryId { get; set; } 
-         public int ProductId { get; set; }
-    }
-}
