e52778e9dd2417c2b3757a3927a939f36317fef0
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-07-31 13:50:35 +0300

    Add files via upload

---
+++ 11.Best-Practices-And-Architecture/RealEstates/RealEstates.Importer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RealEstates.Data;
+using RealEstates.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace RealEstates.Importer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportJsonFile("imot.bg-houses-Sofia-raw-data-2021-03-18.json");
+            Console.WriteLine();
+            ImportJsonFile("imot.bg-raw-data-2021-03-18.json");
+
+        }
+        public static void ImportJsonFile(string fileName)
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+
+            IPropertieService propertiesService = new PropertiesService(dbContext);
+            IEnumerable<PropertyAsJson> properties = JsonSerializer.Deserialize<IEnumerable<PropertyAsJson>>(File.ReadAllText(fileName));
+
+            foreach (PropertyAsJson property in properties)
+            {
+                propertiesService.Add(property.District, property.Floor, property.TotalFloors, property.Size,
+                    property.YardSize, property.Year, property.Type, property.BuildingType, property.Price);
+                Console.Write(".");
+            }
+        }
+    }
+}
