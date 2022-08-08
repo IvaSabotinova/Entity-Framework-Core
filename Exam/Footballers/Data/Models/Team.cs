68886b3cfc131f817fef2afb00ae91b7a9e52c1b
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-08-08 12:55:16 +0300

    Add files via upload

---
+++ Exam/Footballers/Data/Models/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Footballers.Data.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+            TeamsFootballers = new HashSet<TeamFootballer>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Nationality { get; set; }    
+        public int Trophies { get; set; }    
+        public virtual ICollection<TeamFootballer> TeamsFootballers { get; set; }
+    }
+
+
+}
---
+++ Exam/Footballers/DataProcessor/ImportDto/CoachWithFootballersInputModel.cs
@@ -0,0 +1,50 @@
+﻿using Footballers.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class CoachWithFootballersInputModel
+    {
+        [XmlElement("Name")]
+        [Required]
+        [StringLength(40, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [XmlElement("Nationality")]
+        [Required]
+        public string Nationality { get; set; }
+
+        [XmlArray("Footballers")]
+        public FootballerOfCoachInputModel[] Footballers { get; set; }
+    }
+    [XmlType("Footballer")]
+    public class FootballerOfCoachInputModel
+    {
+        [Required]
+        [StringLength(40, MinimumLength =2)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [XmlElement("ContractStartDate")]
+        public string ContractStartDate { get; set; }
+
+        [Required]
+        [XmlElement("ContractEndDate")]
+        public string ContractEndDate { get; set; }
+
+
+        [Required]
+        [XmlElement("BestSkillType")]
+        [EnumDataType(typeof(BestSkillType))]
+        public string BestSkillType { get; set; }
+
+        [Required]
+        [XmlElement("PositionType")]
+        [EnumDataType(typeof(PositionType))]
+        public string PositionType { get; set; }
+       
+    }
+  }
