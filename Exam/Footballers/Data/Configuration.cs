88825d085b45259f0bb42314c3863f9d7e0417af
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-08-10 16:15:05 +0300

    Delete Exam directory

--- Exam/Footballers/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Footballers.Data
-{
-    public class Configuration
-    {
-        public static string ConnectionString = @"Server=.;Database=Footballers;Trusted_Connection=True";
-    }
-}
